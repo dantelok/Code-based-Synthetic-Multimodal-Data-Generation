341da07ee33d0596ef19d5948e62211086bb87c8
Reubencfernandes <18reuchagasfernandes@gmail.com>
2025-05-05 12:22:20 +0530

    wroking on it

--- app/components/prompt.tsx
+++ app/components/prompt.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { MdFileUpload, MdSend } from "react-icons/md";
 const Prompt = () => {
     const [inputValue, setInputValue] = useState<string>('');
 
@@ -10,14 +11,17 @@ const Prompt = () => {
     <textarea
       value={inputValue}
       onChange={handleChange}
-      placeholder="How can I help you today?"
-      className="chat-input w-full h-16 px-4 py-2 text-white bg-transparent border-none resize-none focus:outline-none"
+      placeholder="Enter your AI prompt here, and upload your CSV or image file to begin."
+      className="chat-input w-full h-16 p-4 text-white bg-transparent border-none resize-none focus:outline-none"
     />
-    <div className="button-group flex gap-4">
-      <button className="add-button bg-gray-700 hover:bg-gray-600 text-white rounded-full p-2">
-        <span className="material-symbols-outlined text-xl">add</span>
+    <div className="button-group flex gap-2 m-2 justify-between">
+      <button className="add-button bg-[#1A262B] hover:bg-[#1A262B]/80 text-white rounded-full p-2">
+        <MdFileUpload className='text-[#2F7491] text-2xl'/>
       </button>
-      </div>
+      <button className="send-button bg-[#2F7491] hover:bg-[#2F7491]/80 text-white rounded-full p-2">
+        <MdSend className='text-[#2F7491] text-2xl'/>
+      </button>
+    </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <div className="">
       {/*this is the part where we will display the message */}
     </div>
-    <div className="fixed bottom-0 left-0 right-0 bg-gray-900 z-50 m-5">
-  <div className="max-w-3xl mx-auto">
+    <div className="fixed bottom-0 left-0 right-0 bg-[#1C1C1E] z-50 m-5 rounded-lg">
+  <div className="">
       <Prompt/>
     </div>
     </div>
